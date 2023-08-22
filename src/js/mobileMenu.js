2f73708b6974fa2b3e675deb79e8d275a0a14686
V1talquaaa <115567744+V1talquaaa@users.noreply.github.com>
2023-08-22 22:05:38 +0200

    filex bug with mobile menu

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -1,16 +1,22 @@
 const menuBtn = document.querySelector('.mobMenuIcon');
 const closeMenuBtn = document.querySelector('.mobileMenuClose');
 const mobileMenu = document.querySelector('.mobileMenu');
+const menuLink = document.querySelectorAll('.mobileMenuLink');
 
 menuBtn.addEventListener('click', (e) => {
     e.preventDefault();
     mobileMenu.classList.remove('isHiden')
     mobileMenu.classList.add('isOpen')
-    console.log('click');
 })
 
 closeMenuBtn.addEventListener('click', () => {
     mobileMenu.classList.remove('isOpen')
     mobileMenu.classList.add('isHiden')
-    console.log('click');
 })
+
+menuLink.forEach(link => {
+    link.addEventListener('click', () => {
+        mobileMenu.classList.remove('isOpen');
+        mobileMenu.classList.add('isHiden');
+    });
+});
