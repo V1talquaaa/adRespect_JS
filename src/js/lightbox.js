2152f8b31ace6b0183ec3f9667c88e2d79ea26f1
V1talquaaa <115567744+V1talquaaa@users.noreply.github.com>
2023-08-22 13:26:30 +0200

    fixed bug with lighbox library

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -1,25 +1,69 @@
-const lightbox = document.createElement('div')
-lightbox.id = 'lightbox'
+
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
 document.body.appendChild(lightbox);
 
-const images = document.querySelectorAll('.libraryImg')
-
-images.forEach(image => {
-    image.addEventListener('click', e => {
-        lightbox.classList.add('active');
-        const img = document.createElement('img')
-        img.src = image.src
-        while (lightbox.firstChild) {
-            lightbox.removeChild(lightbox.firstChild)
-        }
+// Delegate event handling to the parent element
+const library = document.querySelector('.grid');
+library.addEventListener('click', e => {
+    if (e.target.classList.contains('libraryImg')) {
+        const img = document.createElement('img');
+        img.src = e.target.src;
+        lightbox.innerHTML = ''; // Clear existing content
         lightbox.appendChild(img);
-    })
-})
+        lightbox.classList.add('active');
+    }
+});
 
 lightbox.addEventListener('click', e => {
-    if(e.target !== e.currentTarget) {
+    if (e.target !== e.currentTarget) {
         return;
     }
-
     lightbox.classList.remove('active');
-})
+});
+
+const images = [
+    {
+        url: 'https://source.unsplash.com/400x400?mountain',
+        alt:'image'
+    },
+    {
+        url: 'https://source.unsplash.com/400x400?sea',
+        alt:'image'
+    },
+    {
+        url: 'https://source.unsplash.com/400x400?car',
+        alt:'image'
+    },
+    ,    {
+        url: 'https://source.unsplash.com/400x400?landskape',
+        alt:'image'
+    }
+    ,    {
+        url: 'https://source.unsplash.com/400x400?ocean',
+        alt:'image'
+    },
+    {
+        url: 'https://source.unsplash.com/400x400?beach',
+        alt:'image'
+    },
+    {
+        url: 'https://source.unsplash.com/400x400?field',
+        alt:'image'
+    },
+    {
+        url: 'https://source.unsplash.com/400x400?mountain',
+        alt:'image'
+    },
+    {
+        url: 'https://source.unsplash.com/400x400?skyscreaper',
+        alt:'image'
+    }
+];
+
+const loadMoreBtn = document.querySelector('.libraryBtn');
+loadMoreBtn.addEventListener('click', () => {
+    images.forEach(image => {
+        library.insertAdjacentHTML('beforeend', `<img src=${image.url} class="libraryImg"></img>`);
+    });
+});
